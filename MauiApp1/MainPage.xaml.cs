107b5454ecfac20c00df0154ef70cd31ffd92ccd
ilyaDavidov <ilya.davidov63@mail.ru>
2022-10-31 19:44:02 +0200

    w

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MauiApp1.Model;
+using System.Collections.ObjectModel;
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage{
+
+    public ObservableCollection<Messager> Mess { get; set; }
+	public MainPage()
+	{
+		Mess=new ObservableCollection<Messager>();
+		BindingContext = this;
+		InitializeComponent();
+		//DateTime d = DateTime.Now;
+		//DateTime d2 = DateTime.Now;
+		//L.Text = d.ToLocalTime().ToString();
+		//L1.Text = d.ToString("D");
+		//L.Text = d.Subtract(DateTime.Now).ToString();
+		//TimeSpan s = d-DateTime.Now;
+		//DatePicker qw;
+
+	}
+	private void DateSwitch(object sender, ToggledEventArgs e)
+	{
+		if(e.Value == false)
+		{
+			dates.Format = "yyyy dd MMMM";
+		}
+		else
+		{
+			dates.Format = "yy dd MMMM";
+		}
+	}
+
+	private void TimeSwitch(object sender, ToggledEventArgs e)
+	{
+        if (e.Value == false)
+        {
+            times.Format = "hh:m";
+        }
+        else
+        {
+            times.Format = "HH:mm";
+        }
+    }
+
+	private void SetMes(object sender, EventArgs e)
+	{
+		Messager qwerty = new Messager();
+		qwerty.date = dates.Date.ToString(dates.Format.ToString());
+		qwerty.time = times.Time.ToString();
+		qwerty.text = Messange.Text;
+		Mess.Add(qwerty);
+	}
+}
+
---
+++ MauiApp1/Model/Messager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Model
+{
+    public class Messager
+    {
+        public string date { get; set; }
+        public string time { get; set; }
+        public string text { get; set; }
+    }
+}
